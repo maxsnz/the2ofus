16aa9302c3d417b737c2a82def51fa4710847b6c
Max Vorozhcov <max@lookatme.ru>
2016-03-01 08:49:06 +0300

    instagram old id

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,5 +1,9 @@
 Instagram.configure do |config|
-  config.client_id     = 'f9f6d4aec83e46a1b4c6d4049a05f967'
-  config.client_secret = '38fa2e78f7684c72a53d569df26dd91d'
-  config.access_token  = '40791087.f9f6d4a.0403c70f1dbb45dfa0ac8a81a13069b7'
+  # config.client_id     = 'f9f6d4aec83e46a1b4c6d4049a05f967'
+  # config.client_secret = '38fa2e78f7684c72a53d569df26dd91d'
+  # config.access_token  = '40791087.f9f6d4a.0403c70f1dbb45dfa0ac8a81a13069b7'
+
+  config.client_id     = 'd31a875f7ff645feabb0a58ef46ce06d'
+  config.client_secret = '1fe0e83f630b4e82bedcb9a745b4b740'
 end
+
