179147fbebffad95f96715828094472d90858f51
Max Vorozhcov <max@lookatme.ru>
2016-03-11 14:58:51 +0300

    fb api fix

--- app/lib/fb_api.rb
+++ app/lib/fb_api.rb
@@ -2,5 +2,5 @@ class FbApi
   include HTTParty
 
   base_uri 'https://graph.facebook.com/v2.3'
-  default_params access_token: Apps.fb.access_token
+  default_params access_token: Apps.facebook.access_token
 end
