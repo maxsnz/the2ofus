d7fe408cde1c93aaa9df2ffd4c8aef4daaa9a354
Max Vorozhcov <max@lookatme.ru>
2016-03-14 16:05:59 +0300

    share pic

--- app/services/import_photos.rb
+++ app/services/import_photos.rb
@@ -1,7 +1,7 @@
 class ImportPhotos
-  def self.call(tag: "the2ofus", mindate: "2016-01-01") # минимальная дата
+  def self.call(tag: "the2ofus", mindate: "2016-03-14") # минимальная дата
 
-    # ImportInstagram.call tag: tag, mindate: mindate
+    ImportInstagram.call tag: tag, mindate: mindate
     ImportVkontakte.call tag: tag, mindate: mindate
   end
 end
